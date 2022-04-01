3f635b7ce988d6a7d8750c20e092b9dbddfaab25
HannaAMcNeil <97768729+HannaAMcNeil@users.noreply.github.com>
2022-04-01 17:03:28 -0400

    Add files via upload

---
+++ lab9/data/megacities.js
@@ -0,0 +1,288 @@
+var geojsonData = 
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Tokyo",
+        "population ": 37400068
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13,
+          35
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Delhi",
+        "population ": 28514000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81,
+          24
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Shanghai",
+        "population ": 25582000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121,
+          31
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "São Paulo",
+        "population ": 21650000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -47,
+          -132
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Mexico City",
+        "population ": 21581000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -99,
+          19
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Cairo",
+        "population ": 20076000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          3,
+          3
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Mumbai",
+        "population ": 19980000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          72,
+          1
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Beijing",
+        "population ": 19618000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          116,
+          39
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Dhaka",
+        "population ": 19578000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9,
+          237
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Osaka",
+        "population ": 19281000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13,
+          34
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "New York",
+        "population ": 18819000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -739,
+          4
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Karachi",
+        "population ": 15400000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          67,
+          24
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Buenos Aires",
+        "population ": 14967000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -58,
+          -34
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Chongqing",
+        "population ": 14838000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          106,
+          29
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Istanbul",
+        "population ": 14751000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          2,
+          4
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Kolkata",
+        "population ": 14681000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          88,
+          22
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Manila",
+        "population ": 13482000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          1,
+          146
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Lagos",
+        "population ": 13463000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          339,
+          645
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Rio de Janeiro",
+        "population ": 13293000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -43,
+          -22
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "city": "Tianjin",
+        "population ": 13215000
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          117,
+          39
+        ]
+      }
+    }
+  ]
+}
+
+
